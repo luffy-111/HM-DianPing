3e8bf918f71c6177729573b0c0c594273e9f3bff
luffy-111 <3122635710@qq.com>
2026-05-28 12:22:55 +0800

    发送验证码业务功能开发

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,11 +1,14 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 虎哥
@@ -13,4 +16,11 @@ import com.hmdp.entity.User;
  */
 public interface IUserService extends IService<User> {
 
+    /**
+     * 发送验证码
+     *
+     * @param phone 手机号
+     * @return 验证码发送结果
+     */
+    Result sendCode(String phone, HttpSession session);
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -15,6 +21,31 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送验证码
+     *
+     * @param phone 手机号
+     * @return 验证码发送结果
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1. 校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2. 如果手机号不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3. 如果手机号符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 4. 保存验证码到session
+        session.setAttribute("code", code);
+
+        // 5. 返回验证码结果
+        log.debug("发送验证码成功，验证码为：{}", code);
+
+        return Result.ok();
+    }
 }
