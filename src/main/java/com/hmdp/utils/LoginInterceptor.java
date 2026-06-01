a29c292e9dd74f65b4486d9253308ff0e72b6b7b
luffy-111 <3122635710@qq.com>
2026-06-01 21:43:41 +0800

    解决状态刷新的问题

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -16,37 +16,15 @@ import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-    private StringRedisTemplate stringRedisTemplate;
-
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1. 获取请求头中的token
-        String token = request.getHeader("authorization");
-        if (token == null || token.isEmpty()) {
-            // 不存在，拦截
+        // 1. 判断是否要拦截(ThreadLocal中是否有用户)
+        if (UserHolder.getUser() == null) {
+            // 拦截，设置状态码
             response.setStatus(401);
             return false;
         }
-        // 2. 基于token获取redis中的用户
-        Map<Object, Object> userMap = stringRedisTemplate.opsForHash()
-                .entries(LOGIN_USER_KEY + token);
-        // 3. 判断用户是否存在
-        if (userMap.isEmpty()) {
-            // 4. 不存在，拦截
-            response.setStatus(401);
-            return false;
-        }
-        // 5. 将查询到的Hash数据转为UserDTO对象
-        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
-        // 6. 存在，保存到ThreadLocal
-        UserHolder.saveUser(userDTO);
-        // 7. 刷新token的TTL
-        stringRedisTemplate.expire(LOGIN_USER_KEY + token, LOGIN_USER_TTL, TimeUnit.MINUTES);
-        // 8. 放行
+        // 放行
         return true;
     }
 
