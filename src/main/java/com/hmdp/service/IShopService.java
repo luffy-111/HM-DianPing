fd745d2f8f335690162ae8a5da54c87872f4bbe7
luffy-111 <3122635710@qq.com>
2026-06-06 18:07:18 +0800

    解决数据库和redis中数据的一致性

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -21,4 +21,12 @@ public interface IShopService extends IService<Shop> {
      * @return 商铺详情数据
      */
     Result queryById(Long id);
+
+    /**
+     * 修改商铺信息
+     *
+     * @param shop 商铺数据
+     * @return 商铺id
+     */
+    Result update(Shop shop);
 }
